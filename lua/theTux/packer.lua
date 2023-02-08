87db06b23f3ac9d548e433ff9276614124705787
Israel Peter Alvarez Castro <aisraelpeter@gmail.com>
2023-02-08 02:47:16 -0400

    mode zen installation

--- lua/theTux/packer.lua
+++ lua/theTux/packer.lua
@@ -4,58 +4,67 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	use('wbthomason/packer.nvim') -- packer manages plugins of Neovim
+  -- packer manages plugins of Neovim
+	use('wbthomason/packer.nvim') 
+  
+
+	--color scheme
+  use { 'svrana/neosolarized.nvim',
+    requires = { 'tjdevries/colorbuddy.nvim' }
+  }
+	use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use( 'nvim-treesitter/playground')
+
+
+  --LSP Support
+  use('neovim/nvim-lspconfig') --LSP
   use('williamboman/mason.nvim')
   use('williamboman/mason-lspconfig.nvim')
 
-	--color scheme
-  -- scheme solarized
-  use {
-    'svrana/neosolarized.nvim',
-    requires = { 'tjdevries/colorbuddy.nvim' }
-  }
-
-  use 'hoob3rt/lualine.nvim' --statusLIne
-
-	use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-
-	use( 'nvim-treesitter/playground')
-
-  use('neovim/nvim-lspconfig') --LSP
-  
-  use('onsails/lspkind-nvim') --vscode-like pictograms
-
+  -- Autocompletion
   use('hrsh7th/cmp-buffer') -- nvim-cmp source for buffer words
-
   use('hrsh7th/cmp-nvim-lsp') -- nvim-cmp source for neovim's built-in LSP
-
   use('hrsh7th/nvim-cmp') -- Completion
+  use('hrsh7th/cmp-path')
+  use('saadparwaiz1/cmp_luasnip')
+  use('hrsh7th/cmp-nvim-lua')
 
-	use( 'tpope/vim-fugitive')
-
+  -- snippets
   use('L3MON4D3/LuaSnip')
+  use('rafamadriz/friendly-snippets')
 
+
+  
+  --statusLIne and bufferline
+  use('hoob3rt/lualine.nvim') 
+  use('akinsho/nvim-bufferline.lua')
+  use('folke/zen-mode.nvim')
+  
+  -- autopairs and icons
   use('windwp/nvim-autopairs')
-
   use('windwp/nvim-ts-autotag')
-
-  use('nvim-lua/plenary.nvim')-- Common utilities
-
-  use('nvim-telescope/telescope.nvim')
-
-  use('nvim-telescope/telescope-file-browser.nvim')
-
   use('kyazdani42/nvim-web-devicons')
 
-  use('akinsho/nvim-bufferline.lua')
-
-  use('glepnir/lspsaga.nvim') -- LSP UIs 
-
+  --Code Formattier
   use('jose-elias-alvarez/null-ls.nvim') --use neovim as a language server to inject LSP diagnostics,code actions
-
   use('MunifTanjim/prettier.nvim') --Prettier plugin for Neovim's builting
 
+
+  --find file telescope
+  use('nvim-telescope/telescope.nvim')
+  use('nvim-telescope/telescope-file-browser.nvim')
+
+
+  --GitHub
+	use( 'tpope/vim-fugitive')
   use('lewis6991/gitsigns.nvim')
+
+
+  use('onsails/lspkind-nvim') --vscode-like pictograms
+  use('nvim-lua/plenary.nvim')-- Common utilities
+  use('glepnir/lspsaga.nvim') -- LSP UIs 
+
+
 
 end)
 
