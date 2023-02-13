75b24eaa4ec69f43e1c19afbb658d84e924f4a60
Israel Peter Alvarez Castro <aisraelpeter@gmail.com>
2023-02-13 06:47:09 -0400

    adding neovim(packer) configuration and color indentation

---
+++ nvim/after/plugin/indent.lua
@@ -0,0 +1,27 @@
+vim.opt.termguicolors = true
+
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#cc241d gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guifg=#98971a gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent3 guifg=#d79921 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent4 guifg=#458588 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent5 guifg=#689d6a gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent6 guifg=#d65d0e gui=nocombine]]
+
+vim.opt.list = true
+--vim.opt.listchars:append "space:⋅"
+--vim.opt.listchars:append "eol:↴"
+
+require("indent_blankline").setup {
+    show_end_of_line = true,
+    space_char_blankline = " ",
+    show_current_context = true,
+    --show_current_context_start = true,
+    char_highlight_list = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+        "IndentBlanklineIndent3",
+        "IndentBlanklineIndent4",
+        "IndentBlanklineIndent5",
+        "IndentBlanklineIndent6",
+    },
+}
