1b36203d2b50d276b8c440f4d8e0c084ec54ebf3
PeterAlvarez <aisraelpeter@gmail.com>
2023-02-17 17:00:41 -0400

    install surround and hop.nvim

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,13 +2,14 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 lsp.ensure_installed({
-    'tsserver',
-    'eslint',
-    'sumneko_lua',
+  'tsserver',
+  'eslint',
+  --'sumneko_lua',
+  --'lua_ls'
 })
 
 lsp.setup_servers({
-    'tsserver'
+  'tsserver'
 })
 
 lsp.set_preferences({
@@ -19,17 +20,17 @@ lsp.set_preferences({
 lsp.preset('recommended')
 
 lsp.setup_nvim_cmp({
-    formatting = {
-        -- changing the order of fields so the icon is the first
-        fields = { 'abbr', 'menu', 'kind' },
-        -- here is where the change happens
-        format = require('lspkind').cmp_format({
-            maxwidth = 80,
-            before = function(entry, vim_item)
-              return vim_item
-            end
-        })
-    },
+  formatting = {
+    -- changing the order of fields so the icon is the first
+    fields = { 'abbr', 'menu', 'kind' },
+    -- here is where the change happens
+    format = require('lspkind').cmp_format({
+      maxwidth = 80,
+      before = function(entry, vim_item)
+        return vim_item
+      end
+    })
+  },
 })
 
 lsp.setup()
@@ -43,12 +44,12 @@ for type, icon in pairs(signs) do
 end
 
 vim.diagnostic.config({
-    virtual_text = {
-        --prefix = '●'
-        prefix = ''
-    },
-    update_in_insert = true,
-    float = {
-        source = "always", -- Or "if_many"
-    },
+  virtual_text = {
+    --prefix = '●'
+    prefix = ''
+  },
+  update_in_insert = true,
+  float = {
+    source = "always", -- Or "if_many"
+  },
 })
--- nvim/after/plugin/lspServerConfig.lua
+++ nvim/after/plugin/lspServerConfig.lua
@@ -7,11 +7,11 @@ local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
 local enable_format_on_save = function(_, bufnr)
   vim.api.nvim_clear_autocmds({ group = augroup_format, buffer = bufnr })
   vim.api.nvim_create_autocmd("BufWritePre", {
-      group = augroup_format,
-      buffer = bufnr,
-      callback = function()
-        vim.lsp.buf.format({ bufnr = bufnr })
-      end,
+    group = augroup_format,
+    buffer = bufnr,
+    callback = function()
+      vim.lsp.buf.format({ bufnr = bufnr })
+    end,
   })
 end
 
@@ -34,92 +34,92 @@ end
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 nvim_lsp.tsserver.setup {
-    on_attach = on_attach,
-    cmd = { "typescript-language-server", "--stdio" },
-    capabilities = capabilities
+  on_attach = on_attach,
+  cmd = { "typescript-language-server", "--stdio" },
+  capabilities = capabilities
 }
 
 nvim_lsp.html.setup {
-    cmd = { "vscode-html-language-server", "--stdio" },
-    on_attach = on_attach,
-    filetypes = { 'html' },
-    capabilities = capabilities,
+  cmd = { "vscode-html-language-server", "--stdio" },
+  on_attach = on_attach,
+  filetypes = { 'html' },
+  capabilities = capabilities,
 
-    init_option = {
-        configurationSection = { "html", "css", "javascript" },
-        embeddedLanguages = {
-            css = true,
-            javascript = true
-        },
-        provideFormatter = true
+  init_option = {
+    configurationSection = { "html", "css", "javascript" },
+    embeddedLanguages = {
+      css = true,
+      javascript = true
     },
-    setting = {},
+    provideFormatter = true
+  },
+  setting = {},
 }
 
-nvim_lsp.sumneko_lua.setup {
-    capabilities = capabilities,
-    on_attach = function(client, bufnr)
-      on_attach(client, bufnr)
-      enable_format_on_save(client, bufnr)
-    end,
-    settings = {
-        Lua = {
-            diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = { 'vim' },
-            },
+nvim_lsp.lua_ls.setup {
+  capabilities = capabilities,
+  on_attach = function(client, bufnr)
+    on_attach(client, bufnr)
+    enable_format_on_save(client, bufnr)
+  end,
+  settings = {
+    Lua = {
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
 
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
-                checkThirdParty = false
-            },
-        },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false
+      },
     },
+  },
 }
 
 nvim_lsp.emmet_ls.setup {
-    cmd = { "emmet-ls", "--stdio" },
-    capabilities = capabilities,
-    on_attach = on_attach,
+  cmd = { "emmet-ls", "--stdio" },
+  capabilities = capabilities,
+  on_attach = on_attach,
 }
 
 
 nvim_lsp.cssls.setup {
-    cmd = { "vscode-css-language-server", "--stdio" },
-    on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = { "css", "scss", "less" },
+  cmd = { "vscode-css-language-server", "--stdio" },
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "css", "scss", "less" },
 }
 
 nvim_lsp.tailwindcss.setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
+  on_attach = on_attach,
+  capabilities = capabilities,
 }
 
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 lsp.setup_servers({
-    'eslint',
-    'angularls',
-    opts = {
-        single_file_support = false,
-        on_attach = function(client, bufnr)
-          print("I'm doing web dev")
-        end
-    }
+  'eslint',
+  'angularls',
+  opts = {
+    single_file_support = false,
+    on_attach = function(client, bufnr)
+      print("I'm doing web dev")
+    end
+  }
 })
 -- reserve space for diagnostic icons
 vim.opt.signcolumn = 'yes'
 
 lsp.setup_servers({
-    'eslint',
-    'angularls',
-    opts = {
-        single_file_support = false,
-        on_attach = function(client, bufnr)
-          print("I'm doing web dev")
-        end
-    }
+  'eslint',
+  'angularls',
+  opts = {
+    single_file_support = false,
+    on_attach = function(client, bufnr)
+      print("I'm doing web dev")
+    end
+  }
 })
