9daf4e6bd5b8c453743962474165dbab89e8e30b
Israel Peter Alvarez Castro <aisraelpeter@gmail.com>
2023-03-21 13:26:58 -0400

    version 1.0 xd

--- scripts/baterry/battery.sh
+++ scripts/baterry/battery.sh
@@ -4,7 +4,7 @@ do
     battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
     if [ $battery_level -ge 95 ]; then
         notify-send -t 5000 "Bateria Casi Llena" "Nivel: ${battery_level}%"
-    elif [ $battery_level -le 72 ]; then
+    elif [ $battery_level -le 20 ]; then
         notify-send -t 10000 --urgency=CRITICAL "Bateria Baja" "Nivel: ${battery_level}%"
         paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
     fi
