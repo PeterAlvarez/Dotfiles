b5d153a477b1ada6a166ab1ef2259769e39f7e94
Israel Peter Alvarez Castro <aisraelpeter@gmail.com>
2023-09-04 05:31:57 -0400

    config telescope lazy

---
+++ nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,11 @@
+return {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    lazy = true,
+    config = true,
+    opts = {
+ --       disable = "TelescopePrompt", "vim",
+    } -- this is equalent to setup({}) function
+}
+
+
