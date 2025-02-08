83ba06b28413f034309705eb083b0dab47dbe80f
Israel Peter Alvarez Castro <63154146+PeterAlvarez@users.noreply.github.com>
2025-02-08 02:44:29 -0400

    bc 2025 ubuntu

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -446,37 +446,51 @@ return {
             "nvim-tree/nvim-web-devicons",
         },
     },
-    --symbols-outline view functions
     {
-        "simrat39/symbols-outline.nvim",
-        keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
-        cmd = "SymbolsOutline",
-        opts = function()
-            local Config = require("lazyvim.config")
-            local defaults = require("symbols-outline.config").defaults
-            local opts = {
-                symbols = {},
-                symbol_blacklist = {},
-            }
-            local filter = Config.kind_filter
-
-            if type(filter) == "table" then
-                filter = filter.default
-                if type(filter) == "table" then
-                    for kind, symbol in pairs(defaults.symbols) do
-                        opts.symbols[kind] = {
-                            icon = Config.icons.kinds[kind] or symbol.icon,
-                            hl = symbol.hl,
-                        }
-                        if not vim.tbl_contains(filter, kind) then
-                            table.insert(opts.symbol_blacklist, kind)
-                        end
-                    end
-                end
-            end
-            return opts
-        end,
+        "hedyhli/outline.nvim",
+        lazy = true,
+        cmd = { "Outline", "OutlineOpen" },
+        keys = { -- Example mapping to toggle outline
+            { "<leader>cn", "<cmd>Outline<CR>", desc = "Toggle outline" },
+        },
+        opts = {
+            outline_window = {
+                position = "left",
+                width = 22,
+            },
+        },
     },
+    --symbols-outline view functions
+    -- {
+    --     "simrat39/symbols-outline.nvim",
+    --     keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    --     cmd = "SymbolsOutline",
+    --     opts = function()
+    --         local Config = require("lazyvim.config")
+    --         local defaults = require("symbols-outline.config").defaults
+    --         local opts = {
+    --             symbols = {},
+    --             symbol_blacklist = {},
+    --         }
+    --         local filter = Config.kind_filter
+    --
+    --         if type(filter) == "table" then
+    --             filter = filter.default
+    --             if type(filter) == "table" then
+    --                 for kind, symbol in pairs(defaults.symbols) do
+    --                     opts.symbols[kind] = {
+    --                         icon = Config.icons.kinds[kind] or symbol.icon,
+    --                         hl = symbol.hl,
+    --                     }
+    --                     if not vim.tbl_contains(filter, kind) then
+    --                         table.insert(opts.symbol_blacklist, kind)
+    --                     end
+    --                 end
+    --             end
+    --         end
+    --         return opts
+    --     end,
+    -- },
 
     --file explorer
     {
