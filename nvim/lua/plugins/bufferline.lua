38636fb6c688a2da942ab3094577138212face8d
Israel Peter Alvarez Castro <aisraelpeter@gmail.com>
2025-08-23 04:11:38 -0400

    nvim
    config lualine

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -47,14 +47,14 @@ return {
   },
 
   --lualine
-  -- {
-  --   "nvim-lualine/lualine.nvim",
-  --   event = "VeryLazy",
-  --   opts = {
-  --     options = {
-  --       -- globalstatus = false,
-  --       -- theme = "solarized_dark",
-  --     },
-  --   },
-  -- },
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        -- globalstatus = false,
+        -- theme = "solarized_dark",
+      },
+    },
+  },
 }
